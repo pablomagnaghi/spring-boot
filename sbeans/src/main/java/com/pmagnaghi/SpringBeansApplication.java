d15be1b00718f0d624a178e5e36d9c92d5337941
Pablo Magnaghi <pmagnaghi@solstice.com>
2017-08-25 17:54:11 -0300

    Add @bean and @service injection and page controller

--- sbeans/src/main/java/com/pmagnaghi/SpringBeansApplication.java
+++ sbeans/src/main/java/com/pmagnaghi/SpringBeansApplication.java
@@ -1,13 +1,22 @@
 package com.pmagnaghi;
 
+import com.abc.foo.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
+@ComponentScan({"com.pmagnaghi","com.abc.foo"})
 @SpringBootApplication
 public class SpringBeansApplication {
+
+    @Bean
+    public User user() {
+        return new User("Dan", "Vega");
+    }
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SpringBeansApplication.class, args);
@@ -17,5 +26,7 @@ public class SpringBeansApplication {
 		for ( String name : beanNames ){
 			System.out.println(name);
 		}
+
+		System.out.println( ctx.getBean("user").toString() );
 	}
 }
