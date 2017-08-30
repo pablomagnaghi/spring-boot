dc49c1171391ca2494eab5a0e49a761483fd5451
Pablo Magnaghi <pmagnaghi@solstice.com>
2017-08-30 14:39:04 -0300

    web-applications starter

---
+++ toysforshots/src/test/java/com/toysforshots/ToysforshotsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.toysforshots;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToysforshotsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
