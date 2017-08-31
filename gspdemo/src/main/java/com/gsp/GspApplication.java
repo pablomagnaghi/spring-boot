6251274d6576baa4baec9bbd4d3fc9a8ab88a5ac
Pablo Magnaghi <pmagnaghi@solstice.com>
2017-08-31 17:53:22 -0300

    Fix gsp

---
+++ gspdemo/src/main/java/com/gsp/GspApplication.java
@@ -0,0 +1,12 @@
+package com.gsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GspApplication.class, args);
+	}
+}
